3b0d40251b9417d81b37fbdeaa7d907139e99042
Oluwafemi Bamisaye <oluwafemi.bamisaye@hellofresh.com>
2022-03-29 16:23:18 +0200

    add delete for deliveries

--- src/main/kotlin/com/jetbrains/handson/httpapi/routes/DeliveryRoutes.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/DeliveryRoutes.kt
@@ -7,6 +7,7 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.eq
 
 fun Route.deliveryRouting(col: CoroutineCollection<Delivery>) {
     route("/delivery") {
@@ -41,6 +42,20 @@ fun Route.deliveryRouting(col: CoroutineCollection<Delivery>) {
             } else {
                 call.respondText("Delivery already added", status = HttpStatusCode.Conflict)
             }
+        }
+
+        delete("{id}") {
+            val deliveryStorage = col.find().toList()
+            val id = call.parameters["id"] ?: return@delete call.respondText(
+                "Missing or malformed id",
+                status = HttpStatusCode.BadRequest
+            )
+            deliveryStorage.find { it.id == id.toInt() } ?: return@delete call.respondText(
+                "No customer with id $id",
+                status = HttpStatusCode.NotFound
+            )
+            col.deleteOne(Delivery::id eq id.toInt())
+            call.respondText("Delivery deleted successfully")
         }
     }
 }
